29e871ce158e11be860b9f8848145aef47356047
Debby <40792376+purple1007@users.noreply.github.com>
2024-08-10 17:10:00 +0000

    Add name of css variables Update layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"; // 新增這行確保這個組件是 Client Component
+"use client"; // Client Component
 
 import { useState } from "react";
 import ColorToken from "../app/pages/ColorTokens";
@@ -7,21 +7,31 @@ import PaltteTokens from "../app/pages/Palette";
 export default function Home() {
   const [activeTab, setActiveTab] = useState("Tab1");
   return (
-    <main className="flex flex-col items-center justify-between p-8 max-w-screen-md m-auto">
+    <main className="flex flex-col items-center justify-between p-8 max-w-screen-lg m-auto">
       <div className="w-full bg-zinc-800 rounded-md shadow px-4">
-        <div className="flex space-x-4 mb-4 border-b pt-2 border-zinc-600">
-          <button
-            className={`px-4 py-2 font-semibold ${activeTab === "Tab1" ? "text-zinx-100 border-b border-zinc-100" : "text-zinc-500"}`}
-            onClick={() => setActiveTab("Tab1")}
-          >
-            Color token
-          </button>
-          <button
-            className={`px-4 py-2 font-semibold ${activeTab === "Tab2" ? "text-zinc-100 border-b border-zinc-100" : "text-zinc-500"}`}
-            onClick={() => setActiveTab("Tab2")}
-          >
-            Palette
-          </button>
+        <div className="fixed w-full left-0 top-0 z-50">
+          <div className="max-w-[960px] flex justify-center m-auto pt-2 bg-zinc-800 border-b border-zinc-600">
+            <button
+              className={`px-4 py-2 font-semibold ${
+                activeTab === "Tab1"
+                  ? "text-zinx-100 border-b border-zinc-100"
+                  : "text-zinc-500"
+              }`}
+              onClick={() => setActiveTab("Tab1")}
+            >
+              Color token
+            </button>
+            <button
+              className={`px-4 py-2 font-semibold ${
+                activeTab === "Tab2"
+                  ? "text-zinc-100 border-b border-zinc-100"
+                  : "text-zinc-500"
+              }`}
+              onClick={() => setActiveTab("Tab2")}
+            >
+              Palette
+            </button>
+          </div>
         </div>
         <div>
           {activeTab === "Tab1" && <ColorToken />}
--- app/pages/ColorTokens.js
+++ app/pages/ColorTokens.js
@@ -2,9 +2,12 @@ import React from "react";
 import colorTokens from "../data/colorTokens.json";
 
 const ColorToken = ({ token }) => (
-  <div className="w-full bg-zinc-800 py-6 px-4 md:p-4 rounded-lg my-1 flex flex-col md:flex-row">
+  <div className="w-full bg-zinc-800 py-8 px-4 md:p-4 rounded-lg my-1 flex flex-col md:flex-row">
     <div className="w-full mr-2 mb-4 md:mb-0">
       <h3 className="text-white font-bold mb-1 md:mb-3">{token.name}</h3>
+      <div className="inline-block bg-zinc-700 px-3 mb-4 mt-2 md:mt-0 rounded-full">
+        <code className="text-gray-300 text-xs">{token.CSS}</code>
+      </div>
       <p className="text-gray-400 text-sm">{token.description}</p>
     </div>
     <div className="flex w-full md:w-30">
@@ -31,19 +34,19 @@ const ColorToken = ({ token }) => (
 const ColorTokens = () => (
   <div className="min-h-screen">
     <div
-      className="sticky top-0 p-2 px-4 mb-2 rounded bg-zinc-800 w-full flex shadow-lg"
+      className="sticky top-12 p-2 px-4 mb-2 pt-4 bg-zinc-800 w-full flex shadow-lg flex-col md:flex-row"
       style={{ justifyContent: "space-between" }}
     >
-      <p className="font-bold ">Color name</p>
+      <p className="font-bold w-full mr-2">Color name</p>
       <div
-        className="flex text-zinc-400 w-[312px] pr-3"
+        className="flex w-full text-zinc-400 pr-3 pt-2 md:pt-0"
         style={{ justifyContent: "space-between" }}
       >
-        <p className="w-32">White value</p>
-        <p className="w-32">Dark value</p>
+        <p className="w-full mr-4">White value</p>
+        <p className="w-full ">Dark value</p>
       </div>
     </div>
-    <div className="flex flex-wrap">
+    <div className="flex flex-wrap py-4">
       {colorTokens.tokens.map((token, index) => (
         <ColorToken key={index} token={token} />
       ))}
